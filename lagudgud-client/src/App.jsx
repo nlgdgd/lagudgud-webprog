e1593ec33275a85285d1c54cdd12b9d58bc46ad3
nlgdgd <nessymaelagudgud@gmail.com>
2026-04-08 01:08:51 +0800

    lab-act1

---
+++ lagudgud-client/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> Welcome to My React App!</h1>
+        <p>
+          Name: [Nessy Mae F. Lagudgud]<br />
+          Email: [nessymaelagudgud@gamil.com]<br />
+          Other Personal Info: <br />
+          Section: [INF 233]<br />
+          Course: [BSIT-MWA]<br />
+        </p>
+      </header>
+    </div>
+  );
+}
+export default App;
